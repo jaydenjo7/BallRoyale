850bbbfde4d05acacb275668f04393f95c0701ca
jaydenjo7 <jaydenjoseph0830@gmail.com>
2023-06-22 20:29:01 -0400

    home screen shows long and lat

--- App.js
+++ App.js
@@ -1,21 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import * as Location from "expo-location";
 
-export default function App() {
+const App = () => {
+  const [location, setLocation] = useState(null);
+
+  useEffect(() => {
+    fetchLocation();
+  }, []);
+
+  const fetchLocation = async () => {
+    try {
+      const { status } = await Location.requestForegroundPermissionsAsync();
+      if (status === "granted") {
+        const { coords } = await Location.getCurrentPositionAsync();
+        const { latitude, longitude } = coords;
+        setLocation({ latitude, longitude });
+      } else {
+        console.log("Location permission denied");
+      }
+    } catch (error) {
+      console.log("Error fetching location:", error.message);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {location ? (
+        <>
+          <Text style={styles.text}>Latitude: {location.latitude}</Text>
+          <Text style={styles.text}>Longitude: {location.longitude}</Text>
+        </>
+      ) : (
+        <Text style={styles.text}>Fetching location...</Text>
+      )}
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 18,
+    marginBottom: 10,
   },
 });
+
+export default App;
